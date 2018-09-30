00356e68e4dbe372e50e3634001ee9bf52f734e6
sagar-kale <sagark.kale@hotmail.com>
2018-09-30 17:28:43 +0530

    removed spring security

--- src/main/java/com/online/seva/Application.java
+++ src/main/java/com/online/seva/Application.java
@@ -1,6 +1,5 @@
 package com.online.seva;
 
-import com.online.seva.config.SpringApplicationContextInitializer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,7 +12,6 @@ public class Application {
     public static void main(String[] args) {
         //
         new SpringApplicationBuilder(Application.class)
-                .initializers(new SpringApplicationContextInitializer())
                 .application()
                 .run(args);
 
