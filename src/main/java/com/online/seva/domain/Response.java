9b542d3a0ab6fe50fe15f273743fbf1d43bfa616
sagar-kale <sagark.kale@hotmail.com>
2018-10-08 12:45:52 +0530

    excluded null values from de-serialising in Response.java

--- src/main/java/com/online/seva/domain/Response.java
+++ src/main/java/com/online/seva/domain/Response.java
@@ -1,7 +1,10 @@
 package com.online.seva.domain;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.util.List;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Response {
     private User user;
     private String message;
