9ee125e0cb3c578d3bfef2a98d578f152bf1c946
Sagar <sagark.kale@hotmail.com>
2018-10-17 12:50:05 +0530

    checked logged user before saving and updating jobs

--- src/main/java/com/online/seva/controller/JobController.java
+++ src/main/java/com/online/seva/controller/JobController.java
@@ -2,20 +2,27 @@ package com.online.seva.controller;
 
 import com.online.seva.domain.Job;
 import com.online.seva.domain.Response;
+import com.online.seva.domain.User;
 import com.online.seva.service.JobService;
+import com.online.seva.service.SessionService;
 import com.online.seva.util.AppConstant;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
 @CrossOrigin
 @ResponseBody
+@Slf4j
 @RequestMapping("/jobs")
 public class JobController {
     @Autowired
     private JobService jobService;
+    @Autowired
+    private SessionService sessionService;
 
     @RequestMapping(value = "/all", method = RequestMethod.GET)
     public List<Job> getAllJobs() {
@@ -23,8 +30,21 @@ public class JobController {
     }
 
     @RequestMapping(value = "/save", method = RequestMethod.POST)
-    public Response saveJob(@RequestBody Job job) {
+    public Response saveJob(@RequestBody Job job, HttpServletRequest request) {
         Response response = new Response();
+        User loggedUser = sessionService.getLoggedUser(request);
+        if (null == loggedUser) {
+            response.setMsgType(AppConstant.ERROR);
+            response.setMessage("Session expired .. please login again");
+            return response;
+        }
+        log.info("logged User ::: True");
+        log.info("User Role::" + loggedUser.getRole());
+        if (!sessionService.isAdmin(loggedUser)) {
+            response.setMsgType(AppConstant.ERROR);
+            response.setMessage("You are not allowed to save jobs,Please contact admin");
+            return response;
+        }
 
         if (jobService.saveJob(job)) {
             response.setMessage("Job Details Saved Successfully");
@@ -35,9 +55,23 @@ public class JobController {
         }
         return response;
     }
+
     @RequestMapping(value = "/update", method = RequestMethod.PUT)
-    public Response updateJob(@RequestBody Job job) {
+    public Response updateJob(@RequestBody Job job, HttpServletRequest request) {
         Response response = new Response();
+        User loggedUser = sessionService.getLoggedUser(request);
+        if (null == loggedUser) {
+            response.setMsgType(AppConstant.ERROR);
+            response.setMessage("Session expired .. please login again");
+            return response;
+        }
+        log.info("logged User ::: True");
+        log.info("User Role::" + loggedUser.getRole());
+        if (!sessionService.isAdmin(loggedUser)) {
+            response.setMsgType(AppConstant.ERROR);
+            response.setMessage("You are not allowed to save jobs,Please contact admin!!");
+            return response;
+        }
 
         if (jobService.updateJob(job)) {
             response.setMessage("Job Details Updated Successfully");
