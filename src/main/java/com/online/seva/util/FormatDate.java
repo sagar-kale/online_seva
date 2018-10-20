74e8a65186d5a522b2a0aac5f85eacb991e7698f
Sagar <sagark.kale@hotmail.com>
2018-10-20 11:53:03 +0530

    adjusted date

--- src/main/java/com/online/seva/util/FormatDate.java
+++ src/main/java/com/online/seva/util/FormatDate.java
@@ -5,14 +5,18 @@ import org.springframework.stereotype.Component;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.Locale;
 import java.util.concurrent.atomic.AtomicReference;
 
 @Component
 @Slf4j
 public class FormatDate {
-    private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-    private SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE dd MMMM yyyy");
+    private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE dd MMMM yyyy", Locale.US);
+    private DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.US);
 
     public String getFormatDate(String formatDate) {
 
@@ -38,6 +42,12 @@ public class FormatDate {
         return date.get();
     }
 
+    /**
+     * Incrementing one day cause html5 date is coming one day prior as selected.
+     * @param lastDate
+     * @return
+     */
+
     public boolean isJobEndDateExpired(String lastDate) {
         Date parsedLastDate = null;
         Date currentDate = null;//dateFormat.parse(dateFormat.format(dateFormat.parse(lastDate)));
@@ -45,13 +55,12 @@ public class FormatDate {
             parsedLastDate = sdf.parse(lastDate);
             String curr_date_format = sdf.format(new Date());
             currentDate = sdf.parse(curr_date_format);
-        } catch (ParseException e) {
+            LocalDate localDate = LocalDate.parse(sdf.format(parsedLastDate), dateTimeFormatter).plusDays(1);
+            parsedLastDate = sdf.parse(localDate.toString());
+        } catch (Exception e) {
             e.printStackTrace();
         }
-
         log.info("parsed after format:::" + sdf.format(parsedLastDate));
-
-
         log.info("current date:::" + sdf.format(currentDate));
 
         String formattedString = String.format("job last date= %s is before current date=%s", sdf.format(parsedLastDate), sdf.format(currentDate));
